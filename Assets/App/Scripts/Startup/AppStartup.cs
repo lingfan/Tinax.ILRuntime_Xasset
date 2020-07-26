06ad521da20a8e65c5c75eeb190f67931b8b0f6a
elfhuv <elfhuv@qq.com>
2020-07-26 12:08:02 +0800

    init

---
+++ Assets/App/Scripts/Startup/AppStartup.cs
@@ -0,0 +1,29 @@
+﻿using TinaX;
+using TinaX.I18N;
+using TinaX.Services;
+using UnityEngine;
+
+
+namespace Nekonya.Example
+{
+    public class AppStartup : MonoBehaviour
+    {
+        private async void Start()
+        {
+            var core = XCore.New()
+            //.UseVFS()
+            .RegisterServiceProvider(new XAsset.XAssetProvider())
+            .UseUIKit()
+            .UseI18N()
+            .UseXILRuntime()
+            .OnServicesStartException((service, err) =>
+            {
+                //
+            });
+            await core.RunAsync();
+
+            //startup TinaX Framework 
+        }
+    }
+}
+
---
+++ HotfixProject/Startup/MeowStartup.cs
@@ -0,0 +1,45 @@
+﻿using Nekonya.UI;
+using TinaX;
+//using TinaX.VFSKit;
+using TinaX.UIKit;
+using XAsset;
+using libx;
+using UniRx;
+using UnityEngine;
+
+
+namespace Nekonya
+{
+    public class MeowStartup
+    {
+        public static void Start()
+        {
+            Debug.Log("Meow~~~");
+            //open ui
+            var core = XCore.GetMainInstance();
+            var uikit = core.Services.Get<IUIKit>();
+            uikit.OpenUIAsync("mainScreen", new MainScreen(), (entity, err) =>
+            {
+                if (err != null)
+                {
+                    Debug.LogError("Open mainScreen failed:" + err.Message);
+                }
+            });
+
+            //load scene
+            //var vfs = core.GetService<IVFS>();
+            //vfs.LoadSceneAsync("Assets/App/Scenes/App.Main.unity", (scene, err) =>
+            //{
+            //    scene.OpenScene();
+            //});
+            var scene_req = Assets.LoadSceneAsync("Assets/App/Scenes/App.Main.unity", false);
+            scene_req.ToObservable()
+                .ObserveOnMainThread()
+                .SubscribeOnMainThread()
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Load Scene Success");
+                });
+        }
+    }
+}
